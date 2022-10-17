cf5b1a26dbb16d18608b4e2863519104468ece31
Marwan2310 <imaroneu@gmail.com>
2022-10-17 17:01:38 +0700

    menyelesaikan struktur folder, routes dan view

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog | Posts</title>
+</head>
+
+<body>
+
+    <h1>Halaman Posts</h1>
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Marwan",
+        "email" => "imaroneu@gmail.com",
+        "image" => "me.jpg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
